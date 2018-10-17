aba9886f7f2049925af40794f361908dc1cdeeaa
Joachim Nordeke <joachim@nordeke.se>
2018-10-17 10:04:07 +0200

    Added panels

---
+++ Butik/Butik/MonsterBay.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Butik
+{
+    class MonsterBay : FlowLayoutPanel
+    {
+        public MonsterBay()
+        {
+            Dock = DockStyle.Fill;
+            BorderStyle = BorderStyle.FixedSingle;
+        }
+    }
+}
--- Butik/Butik/MyForm.cs
+++ Butik/Butik/MyForm.cs
@@ -17,6 +17,40 @@ namespace Butik
             Height = 650;
             Text = "the Monsters Bay";
             Icon = new Icon("resources/icon.ico");
+            BackColor = Color.White;
+
+            TableLayoutPanel mainPanel = new TableLayoutPanel()
+            {
+                Dock = DockStyle.Fill,
+                ColumnCount = 2,
+                RowCount = 2
+            };
+            mainPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100));
+            mainPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 300));
+            mainPanel.RowStyles.Add(new RowStyle(SizeType.Absolute, 50));
+            Controls.Add(mainPanel);
+
+            Label titleLabel = new Label()
+            {
+                Font = new Font("Arial", 18),
+                TextAlign = ContentAlignment.MiddleLeft,
+                Text = "the Monster Bay",
+                Dock = DockStyle.Fill
+            };
+            mainPanel.Controls.Add(titleLabel);
+
+            Label cartLabel = new Label()
+            {
+                Font = new Font("Arial", 14),
+                TextAlign = ContentAlignment.MiddleCenter,
+                Text = "the Shopping Cart",
+                Dock = DockStyle.Fill
+            };
+            mainPanel.Controls.Add(cartLabel);
+            
+            mainPanel.Controls.Add(new MonsterBay());
+            
+            mainPanel.Controls.Add(new Cart());
         }
     }
 }
