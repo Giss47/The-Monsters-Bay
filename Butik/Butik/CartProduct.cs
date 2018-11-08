c76b5ee6420ae2c89878cfb3ae50bd9d6fdde86f
Ghassan <ghassan_aidi@hotmail.com>
2018-11-08 09:42:23 +0100

    remove all button added

--- Butik/Butik/CartProduct.cs
+++ Butik/Butik/CartProduct.cs
@@ -3,7 +3,7 @@
     class CartProduct
     {
         public string Name { get; }
-        public int Quantity { get; set; }
+        public int Quantity { get; private set; }
         public double Cost { get; private set; }
 
         // Prevents the Price-variable to be shown in the cart DataGridView.
